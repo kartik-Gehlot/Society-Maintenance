b0c699c293d6825160075fe96cf3d3f03e52eaab
kartik gehlot <kartikgehlot424@gmail.com>
2024-04-21 01:39:58 +0530

    kartik

--- admin/TaxiShow.php
+++ admin/TaxiShow.php
@@ -2,36 +2,15 @@
 include 'includes/shared/header.php';
 include 'includes/shared/sidebar.php';
 include 'includes/shared/topbar.php';
-<<<<<<< HEAD
-$conn = mysqli_connect("localhost", "root", "", "sms") or die ("not connect");
-$qry = "select * from taxi";
-$data = mysqli_query($conn, $qry) or die ("not fire");
-//if (isset ($_SESSION['Admin']) and $_SESSION['status'] == true) {
-=======
 // session_start();
 $conn = mysqli_connect("localhost", "root", "", "sms") or die ("not connect");
 $qry = "select * from taxi";
 $data = mysqli_query($conn, $qry) or die ("not fire");
 // if (isset ($_SESSION['Admin']) and $_SESSION['status'] == true) {
->>>>>>> 9e53f521b7e32c1a0aa28d5020b8bb9375fcd627
     ?>
     <!DOCTYPE html>
     <html lang="en">
 
-<<<<<<< HEAD
-    
-    <body>
-
-        <div class="container my-5">
-            <div class="row">
-                <div class="col-sm-2  text-center ">
-                    <div class="row fs-4">
-
-                       
-                        
-                    </div>
-                </div>
-=======
     <head>
         <?php// include ("../Link.php") ?>
     </head>
@@ -72,7 +51,6 @@ $data = mysqli_query($conn, $qry) or die ("not fire");
                         </div>
                     </div>
                 </div> -->
->>>>>>> 9e53f521b7e32c1a0aa28d5020b8bb9375fcd627
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-12">
@@ -110,10 +88,7 @@ $data = mysqli_query($conn, $qry) or die ("not fire");
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
-=======
         <?php //include ("../Footer.php") ?>
->>>>>>> 9e53f521b7e32c1a0aa28d5020b8bb9375fcd627
 
     </body>
 
@@ -122,16 +97,8 @@ $data = mysqli_query($conn, $qry) or die ("not fire");
     <?php
 // } else {
 //     header("location:Login.php");
-<<<<<<< HEAD
-//     // echo "error";
-// }
-include './includes/shared/footer.php';
-include './includes/shared/scripts.php';
-
-=======
     // echo "error";
 // }
 include './includes/shared/footer.php';
 include './includes/shared/scripts.php';
->>>>>>> 9e53f521b7e32c1a0aa28d5020b8bb9375fcd627
 ?>
